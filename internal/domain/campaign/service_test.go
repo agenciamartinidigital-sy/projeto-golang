630bb1c423aca316d67279afd8eda03c24ee6383
Luís Martini <agenciamartinidigital@gmail.com>
2026-06-11 09:45:10 -0300

    att

--- internal/domain/campaign/service_test.go
+++ internal/domain/campaign/service_test.go
@@ -5,7 +5,17 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
+
+type repositoryMock struct {
+	mock.Mock
+}
+
+func (r *repositoryMock) Save(campaign *Campaign) error {
+	args := r.Called(campaign)
+	return args.Error(0)
+}
 
 func Test_Create_Campaign(t *testing.T) {
 	assert := assert.New(t)
@@ -13,9 +23,9 @@ func Test_Create_Campaign(t *testing.T) {
 	service := Service{}
 
 	newCampaign := contract.NewCampaign{
-		Name: "Teste Y",
+		Name:    "Teste Y",
 		Content: "Body",
-		Emails: []string{"teste1@test.com"},
+		Emails:  []string{"teste1@test.com"},
 	}
 
 	id, err := service.Create(newCampaign)
@@ -26,19 +36,27 @@ func Test_Create_Campaign(t *testing.T) {
 }
 
 func Test_Create_SaveCampaign(t *testing.T) {
-	assert := assert.New(t)
-
-	service := Service{}
-
+	
+	repositoryMock := new(repositoryMock)
 	newCampaign := contract.NewCampaign{
-		Name: "Teste Y",
+		Name:    "Teste Y",
 		Content: "Body",
-		Emails: []string{"teste1@test.com"},
+		Emails:  []string{"teste1@test.com"},
 	}
 
-	id, err := service.Create(newCampaign)
+	repositoryMock.On("Save", mock.MatchedBy(func(campaign *Campaign) bool {
+		if campaign.Name != newCampaign.Name || 
+		campaign.Content != newCampaign.Content || 
+		len(campaign.Contacts) != len(newCampaign.Emails) {
+			return false
+		}
+		return true
+	})).Return(nil)
 
-	assert.NotNil(id)
-	assert.Nil(err)
+	service := Service{repositoryMock}
 
+	service.Create(newCampaign)
+	
+	repositoryMock.AssertExpectations(t)
 }
+//repositoryMock.On("Save", mock.Anything).Return(nil)
