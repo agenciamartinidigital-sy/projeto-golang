b11b67d2dd9b6768c4de4d7af1a573a7cf0540dc
agenciamartinidigital <agenciamartinidigital@gmail.com>
2026-07-06 12:24:26 -0300

    att

--- internal/domain/campaign/campaign.go
+++ internal/domain/campaign/campaign.go
@@ -16,17 +16,17 @@ const (
 
 type Contact struct {
 	ID         string `gorm:"size:50"`
-	Emails     string `validate:"email"`
+	Emails     string `validate:"email" gorm:"size:100"`
 	CampaignID string `gorm:"size:50"`
 }
 
 type Campaign struct {
 	ID        string    `validate:"required" gorm:"size:50"`
-	Name      string    `validate:"min=5,max=24" gorm:"size:"100"`
+	Name      string    `validate:"min=5,max=24" gorm:"size:100"`
 	CreatedOn time.Time `validate:"required"`
 	Content   string    `validate:"min=5,max=1024" gorm:"size:1024"`
 	Contacts  []Contact `validate:"min=1,dive"`
-	Status    string    `gorm:""size:20`
+	Status    string    `gorm:"size:20"`
 }
 
 func NewCampaign(name, content string, emails []string) (*Campaign, error) {
